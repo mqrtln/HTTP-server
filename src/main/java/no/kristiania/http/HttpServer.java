2ab9e1d50d7f980b10b7ffcbaa55342eb7cd264a
Martin Olaussen <maol049@EGMS.no>
2021-10-01 19:24:23 +0200

    Server should return content-type

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -22,45 +22,33 @@ public class HttpServer {
 
             String[] requestLine = HttpClient.readLine(clientSocket).split(" ");
             String requestTarget = requestLine[1];
-            String responseText = "File not found: " + requestTarget;
 
-            String response = "HTTP/1.1 404 Not found\r\n"+
-                    "Content-Length: " + responseText.getBytes().length + "\r\n" +
-                    "\r\n" +
-                    responseText;
+            if(requestTarget.equals("/hello")){
+                String responseText = "<p>Hello world</p>";
+                String response = "HTTP/1.1 200 OK\r\n"+
+                        "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n" +
+                        responseText;
+                clientSocket.getOutputStream().write(response.getBytes());
 
-            clientSocket.getOutputStream().write(response.getBytes());
+            }else {
+                String responseText = "File not found: " + requestTarget;
+
+                String response = "HTTP/1.1 404 Not found\r\n"+
+                        "Content-Length: " + responseText.getBytes().length + "\r\n" +
+                        "\r\n" +
+                        responseText;
+
+                clientSocket.getOutputStream().write(response.getBytes());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(1012);
-
-
-
-        Socket clientSocket = serverSocket.accept();
-
-        String requestLine =  HttpClient.readLine(clientSocket);
-
-        System.out.println(requestLine);
-
-        String headerLine;
-        while (!(headerLine = HttpClient.readLine(clientSocket)).isBlank()){
-            System.out.println(headerLine);
-        }
-
-        String messageBody = "<h1>Hello World</h1>" + "<h2>Benis</h2>";
-        String contentType = "text/html; charset=utf-8";
-
-        String responseMessage = "HTTP/1.1 200 OK\r\n" +
-                "Content-Length: " + messageBody.getBytes().length + "\r\n" +
-                "Content-Type: " + contentType + "\r\n" +
-                "Connection: close\r\n" +
-                "\r\n" +
-                messageBody;
-        clientSocket.getOutputStream().write(responseMessage.getBytes());
+        new HttpServer(1962);
     }
 
     public int getPort() {
--- src/test/java/no/kristiania/http/HttpServerTest.java
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -23,4 +23,15 @@ class HttpServerTest {
         assertEquals("File not found: /non-existing", client.getMessageBody());
     }
 
+    @Test
+    void shouldRespondWith200ForKnownRequestTarget() throws IOException {
+        HttpServer server = new HttpServer(10003);
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/hello");
+        assertAll(
+                () -> assertEquals(200, client.getStatusCode()),
+                () -> assertEquals("text/html", client.getHeader("Content-Type")),
+                () -> assertEquals("<p>Hello world</p>", client.getMessageBody())
+        );
+    }
+
 }
